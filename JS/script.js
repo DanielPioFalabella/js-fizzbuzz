cd16894b69d9483c18e5dba7de3cba8c28fb3793
Daniel Pio Falabella <danielpiofalabella001@gmail.com>
2025-12-02 12:54:16 +0100

    struttura logica di base

---
+++ JS/script.js
@@ -0,0 +1,4 @@
+// scrivo un ciclo di numeri che si ripete da 1 a 100
+// do delle condizioni nel caso il numero è divisibile per 3
+// do delle condizioni nel caso il numero è divisibile per 5
+// do delle condizioni nel caso il numero è divisibile sia per 3 che per 5
