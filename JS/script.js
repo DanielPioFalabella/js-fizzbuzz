a7b2bac81382b5321eabed958e90c2b9934644c9
Daniel Pio Falabella <danielpiofalabella001@gmail.com>
2025-12-02 15:50:05 +0100

    style: stile pagina

--- JS/script.js
+++ JS/script.js
@@ -1,26 +1,31 @@
 // scrivo un ciclo di numeri che si ripete da 1 a 100
 const listaNum = document.getElementById("listaNum");
-let numLi;
+let numLi, classe;
+
 
 for (let i = 1; i <= 100; i++) {
     // do delle condizioni nel caso il numero è divisibile sia per 3 che per 5
     if (i % 3 === 0 && i % 5 === 0) {
         numLi = "FIZZBUZZ";
+        classe = "fizzbuzz"
     }
     // do delle condizioni nel caso il numero è divisibile per 3
     else if (i % 3 === 0) {
         numLi = "FIZZ";
+        classe = "fizz"
     }
     // do delle condizioni nel caso il numero è divisibile per 5
     else if (i % 5 === 0) {
         numLi = "BUZZ";
+        classe = "buzz"
     }
     else {
         numLi = i
+        classe = "color-none"
     }
 
     console.log(numLi);
 
-    listaNum.innerHTML += `<li>${numLi}</li>`
+    listaNum.innerHTML += `<li class="${classe}">${numLi}</li>`;
 }
 
